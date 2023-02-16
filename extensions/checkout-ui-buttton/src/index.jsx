f1eb898fec9f33cabc985e999ca5695b540d9459
theteamisbest <abhithory@gmail.com>
2023-02-16 14:57:18 +0530

    extension added at checkout render before

--- extensions/checkout-ui-buttton/src/index.jsx
+++ extensions/checkout-ui-buttton/src/index.jsx
@@ -1,36 +1,90 @@
+import { useState, useEffect } from 'react';
 import {
   render,
   TextField,
   InlineStack,
+  Text,
   Button,
   Link,
   Modal,
-  TextBlock
+  TextBlock,
+  BlockLayout,
+  View,
+  Heading
 } from '@shopify/checkout-ui-extensions-react';
 
 
 
 render('Checkout::Reductions::RenderAfter', () => (
   <AppDiscount />
+  ));
+  
+  render('Checkout::Dynamic::Render[OrderSummary4]', () => (
+    <RewardFromOrder />
 ));
-render('Checkout::Dynamic::Render', () => (
-  <App />
+
+render('Checkout::Dynamic::Render [OrderSummary4]', () => (
+  <RewardFromOrder />
 ));
+
+
+
+
+function RewardFromOrder() {
+  const [tokenUserWillGet, setTokenUserWillGet] = useState(55)
+  return (
+    <View border="base" padding="base">
+      <Heading>Total Points you will get from this order: {tokenUserWillGet}</Heading>
+      <Heading>Value of your tokens: ${tokenUserWillGet / 4}</Heading>
+      <Text size="small">You get 1 token for order of $1</Text>
+    </View>
+  )
+}
+
+function ModelContent() {
+
+  const [totalPointsUserHave, setTotalPointsUserHave] = useState(11);
+
+  useEffect(() => {
+    setTotalPointsUserHave(4327);
+  }, [])
+
+  return (
+    // <BlockLayout rows={[60, 'fill']}>
+    <BlockLayout>
+      <View border="base" padding="base">
+        <Heading>Total Points you have: {totalPointsUserHave}</Heading>
+        <Heading>Total value of tokens: ${totalPointsUserHave / 4}</Heading>
+        <Text size="small">4 tokens is equal to $1</Text>
+      </View>
+      <Text size="small">You can use Maximam 200 tokens in this order</Text>
+      <InlineStack columns={['70%', 'fill']}>
+        <TextField label="How much tokens do you want to apply?" />
+        <Button>
+          Apply Tokens
+        </Button>
+      </InlineStack>
+    </BlockLayout>
+  )
+}
 
 
 
 function AppDiscount() {
   return (
-    <InlineStack>
-  
-      <Button
-        onPress={() => {
-          console.log('button was pressed');
-        }}
+    <BlockLayout>
+      <Link
+        overlay={
+          <Modal padding title="Apply Discount with Loyaly points">
+            <ModelContent />
+          </Modal>
+        }
       >
-        Apply Discount with Your token
-      </Button>
-    </InlineStack>
+        <Button>
+          Apply Discount with Your token
+        </Button>
+      </Link>
+    </BlockLayout>
   );
 }
 
@@ -64,8 +118,8 @@ function App() {
         }
       >
         <Button>
-        Popup
-      </Button>
+          Popup
+        </Button>
       </Link>
     </InlineStack>
   );
